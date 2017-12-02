ef7c974d0da38223503d6845ec363c8630f2c032
Makro <markotaht@gmail.com>
2017-12-02 14:50:13 +0200

    GameManager and room controller

--- LudumDare40/Assets/Scripts/RoomController.cs
+++ LudumDare40/Assets/Scripts/RoomController.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class RoomController : MonoBehaviour {
 
-    [SerializeField] private RoomSwitch[] doors;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    [SerializeField] private Door[] doors;
+
+    [SerializeField] private int MobCount = 6;
+
+    [SerializeField] private int roomId = 1;
 
     public void Load(RoomController previousRoom) {
         Vector3 playerPosition;
-        foreach(RoomSwitch s in doors)
+        foreach(Door s in doors)
         {
             if(s.NextRoom == previousRoom) {
                 playerPosition = s.transform.position;
@@ -21,9 +21,22 @@ public class RoomController : MonoBehaviour {
             }
         }
         gameObject.SetActive(true);
+        GameManager.instance.CurrentRoom = this;
     }
 
     public void Unload() {
         gameObject.SetActive(false);
+    }
+
+    public void DecreaseMobCounter(int dif)
+    {
+        MobCount -= dif;
+        if(MobCount == 0)
+        {
+            foreach (Door d in doors)
+            {
+                d.OpenDoor();
+            }
+        }
     }
 }
