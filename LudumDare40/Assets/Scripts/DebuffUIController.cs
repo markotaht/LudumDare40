16ff277698096a03feef176131055eaa2605191e
Diana <dianx93@gmail.com>
2017-12-02 23:59:25 +0200

    debuff effect

--- LudumDare40/Assets/Scripts/DebuffUIController.cs
+++ LudumDare40/Assets/Scripts/DebuffUIController.cs
@@ -8,7 +8,7 @@ public class DebuffUIController : MonoBehaviour {
     [SerializeField] GameObject BuffImagePrefab;
     private Dictionary<PlayerStats.Buff, GameObject> _debuffs = new Dictionary<PlayerStats.Buff, GameObject>();
 
-    public void SetBuff(PlayerStats.Buff debuff, int count)
+    public void SetBuff(PlayerStats.Buff debuff, int count, bool increase)
     {
         GameObject go;
         if (_debuffs.ContainsKey(debuff))
@@ -23,11 +23,44 @@ public class DebuffUIController : MonoBehaviour {
 
         if(count == 0)
         {
-            _debuffs.Remove(debuff);
-            Destroy(go);
+            StartCoroutine(BuffRemoveEffect(go.GetComponent<Image>(), true, debuff));
             return;
         }
+        if(increase) StartCoroutine(BuffAddEffect(go.GetComponent<Image>()));
+        else StartCoroutine(BuffRemoveEffect(go.GetComponent<Image>(), false, debuff));
         Text _count = go.GetComponentInChildren<Text>();
         _count.text = count.ToString();
+    }
+    
+    public IEnumerator BuffAddEffect(Image img)
+    {
+        img.color = Color.red;
+        float tParam = 0;
+        float speed = 2f;
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speed;
+            img.color = Color.Lerp(Color.red, Color.white, tParam);
+            yield return null;
+        }
+
+    }
+    public IEnumerator BuffRemoveEffect(Image img, bool destroy, PlayerStats.Buff buffToDestroy)
+    {
+        img.color = Color.green;
+        float tParam = 0;
+        float speed = 2f;
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speed;
+            img.color = Color.Lerp(Color.green, Color.white, tParam);
+            yield return null;
+        }
+        if (destroy)
+        {
+            _debuffs.Remove(buffToDestroy);
+            Destroy(img.gameObject);
+        }
+
     }
 }
