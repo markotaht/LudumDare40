c016419e4ffb862444276b0260f190bde1f97087
Martin <martinviidik@gmail.com>
2017-12-02 11:59:51 +0200

    -Placeholders -Character controller -Player stats

---
+++ LudumDare40/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 _direction;
+
+    PlayerStats playerStats;
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        playerStats = GetComponent<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _direction = transform.TransformDirection(_direction);
+        _direction *= playerStats._speed;
+
+        controller.Move(_direction * Time.deltaTime);
+    }
+}
