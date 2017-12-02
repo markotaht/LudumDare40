00d181da492ef4d1ac928a2c4ea7e56c4601873a
Diana <dianx93@gmail.com>
2017-12-02 12:48:48 +0200

    rat ai

---
+++ LudumDare40/Assets/Scripts/AI/RatAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatAI : MonoBehaviour {
+
+    public GameObject Player;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector3.Distance(transform.position, Player.transform.position) > 0.2f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
+	}
+}
