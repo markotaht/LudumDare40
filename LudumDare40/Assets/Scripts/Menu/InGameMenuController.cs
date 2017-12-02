7f9eabfa0cdabb21b034288ba515f24aa03c1543
Makro <markotaht@gmail.com>
2017-12-02 15:40:33 +0200

    Maine menu logic + door tests

---
+++ LudumDare40/Assets/Scripts/Menu/InGameMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InGameMenuController : MonoBehaviour {
+
+    [SerializeField] private GameObject menuPanel;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuPanel.SetActive(!menuPanel.activeInHierarchy);
+            if (menuPanel.activeInHierarchy)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        menuPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
