329eba62b4fc6eb99350217191be996c73aeab7b
Martin <martinviidik@gmail.com>
2017-12-03 18:07:12 +0200

    -food works -small fixes

---
+++ LudumDare40/Assets/Scripts/Food.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerStats playerStats = other.GetComponent<PlayerStats>();
+        if (playerStats)
+        {
+            playerStats.AddBuff(PlayerStats.Buff.Dysentery);
+            Destroy(gameObject);
+        }
+    }
+
+}
