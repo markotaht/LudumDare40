0060aa63083a4952305c00fdda52a87b074744c4
Makro <markotaht@gmail.com>
2017-12-03 12:28:03 +0200

    Floating texts

---
+++ LudumDare40/Assets/Scripts/FloatingBuff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingBuff : MonoBehaviour {
+
+    [SerializeField] private Image _buffSprite;
+    [SerializeField] private Image _opSprite;
+
+    public void SetSprites(Sprite buff, Sprite op)
+    {
+        _buffSprite.sprite = buff;
+        _opSprite.sprite = op;
+    }
+
+    public void Disappear()
+    {
+        Destroy(gameObject);
+    }
+}
