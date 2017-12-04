ad06190b7afbdefa34ba8a421e47ccb1b5f2f495
Martin <martinviidik@gmail.com>
2017-12-04 14:02:56 +0200

    workin on main scene

--- LudumDare40/Assets/Scripts/Trap.cs
+++ LudumDare40/Assets/Scripts/Trap.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Trap : MonoBehaviour {
 
     public PlayerStats.Buff _buff;
+    public Sprite activatedSpikes;
+    public Sprite disabledSpikes;
+    public SpriteRenderer spikeCosmetic;
+    bool enabled;
 
 	// Use this for initialization
 	void Start () {
-		
+        enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (enabled){
+            spikeCosmetic.sprite = activatedSpikes;
+        } else {
+            spikeCosmetic.sprite = disabledSpikes;
+        }
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -21,7 +29,16 @@ public class Trap : MonoBehaviour {
         PlayerStats playerStats = other.GetComponent<PlayerStats>();
         if (playerStats)
         {
+            enabled = true;
             playerStats.AddBuff(_buff);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerStats playerStats = other.GetComponent<PlayerStats>();
+        if (playerStats)
+        {
+            enabled = false;
         }
     }
 }
