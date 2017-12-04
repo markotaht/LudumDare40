4a8d0dd7d74c8a06bfb2ca04d1acc0cb8e66c779
Makro <markotaht@gmail.com>
2017-12-04 10:52:43 +0200

    Fix

--- LudumDare40/Assets/Scripts/Food.cs
+++ LudumDare40/Assets/Scripts/Food.cs
@@ -12,7 +12,8 @@ public class Food : MonoBehaviour {
             if (Random.Range(0, 100) < 50)
                 playerStats.AddBuff(PlayerStats.Buff.Dysentery);
                 playerStats.AddBuff(PlayerStats.Buff.Weakness);
-        } else playerStats.Heal(20);
+        } else{
+            playerStats.Heal(20);
             Destroy(gameObject);
         }
     }
